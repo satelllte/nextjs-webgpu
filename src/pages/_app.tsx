71db1d5fc253cf7c16e9c9aa4e74229f6c85ca20
George Satellite <lunaerxs@gmail.com>
2022-05-26 15:11:07 +0000

    cleanup: WASM

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { WASMContextProvider } from '../context/WASM'
-import { WASMWorkerContextProvider } from '../context/WASMWorker'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <WASMContextProvider>
-      <WASMWorkerContextProvider>
-        <Component {...pageProps} />
-      </WASMWorkerContextProvider>
-    </WASMContextProvider>
+    <Component {...pageProps} />
   )
 }
 
