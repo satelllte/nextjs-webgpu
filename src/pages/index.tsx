806efd4b0a2dee9ff311aa8c1e632b87cef6dd3f
George Satellite <lunaerxs@gmail.com>
2022-05-26 16:14:02 +0000

    tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,21 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Next.JS with WebGPU</title>
         <meta name="description" content="Next.JS with WebGPU" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js</a> with WebGPU!
+      <main className='min-h-screen p-4 flex flex-col justify-center'>
+        <h1 className='text-4xl text-center font-bold'>
+          Welcome to <a className='text-blue-600 hover:underline' href="https://nextjs.org">Next.js</a> with WebGPU!
         </h1>
       </main>
-    </div>
+    </>
   )
 }
 
