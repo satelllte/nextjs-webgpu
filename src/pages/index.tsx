39890ada60f9c06a176188e33bbcaef337b42eb0
George Satellite <lunaerxs@gmail.com>
2022-05-30 22:44:14 +0000

    tiny style improvements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className='min-h-screen p-4 flex flex-col justify-between'>
-        <h1 className='text-3xl sm:text-4xl lg:text-5xl text-center font-bold my-4 sm:my-8 lg:my-12'>
+        <h1 className='text-3xl sm:text-4xl lg:text-5xl text-center font-bold my-8 lg:my-12'>
           Welcome to <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> with WebGPU!
         </h1>
         <section className='flex-1 flex justify-center items-center'>
